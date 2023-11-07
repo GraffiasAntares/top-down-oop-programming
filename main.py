22eaccb24f7f8a99745a63f6ca4b6a03aa9f1205
GraffiasAntares <star211@list.ru>
2023-11-07 22:36:16 +0700

    Added object connection and create/info object functions

--- main.py
+++ main.py
@@ -8,11 +8,43 @@ class Van:
         self.van_number = van_number
         self.seats = []
 
+    def add_seat(self, seat_number):
+        seat = Seat(seat_number)
+        self.seats.append(seat)
+
 
 class Train:
     def __init__(self, train_number):
         self.train_number = train_number
         self.vans = []
+
+    def add_van(self, van_number):
+        van = Van(van_number)
+        self.vans.append(van)
+
+
+def create_train(trains_list):
+    pass
+
+
+def create_van(trains_list):
+    pass
+
+
+def create_seat(trains_list):
+    pass
+
+
+def show_objects(trains_list):
+    pass
+
+
+def train_info(train):
+    pass
+
+
+def van_info(van):
+    pass
 
 
 def main():
