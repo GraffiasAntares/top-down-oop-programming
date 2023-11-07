cdbd7cf212cc5517dec7f0984cb727699d6864b9
GraffiasAntares <star211@list.ru>
2023-11-07 22:04:41 +0700

    Basic objects

---
+++ main.py
@@ -0,0 +1,25 @@
+class Seat:
+    def __init__(self, seat_number):
+        self.seat_number = seat_number
+
+
+class Van:
+    def __init__(self, van_number):
+        self.van_number = van_number
+        self.seats = []
+
+
+class Train:
+    def __init__(self, train_number):
+        self.train_number = train_number
+        self.vans = []
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
